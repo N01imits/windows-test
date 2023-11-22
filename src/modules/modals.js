f21db463890916225eb55ff4403f6a731d6ab93f
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-11-22 09:44:43 +0300

    code improvements

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -1,42 +1,49 @@
 const modals = () => {
 	function bindModal(triggerSelector, modalSelector, closeSelector) {
-		const trigger = document.querySelectorAll(triggerSelector),
-			modal = document.querySelector(modalSelector),
-			close = document.querySelector(closeSelector);
+		const triggers = document.querySelectorAll(triggerSelector);
+		const modal = document.querySelector(modalSelector);
+		const close = document.querySelector(closeSelector);
 
-		trigger.forEach(item => {
-			item.addEventListener('click', e => {
+		const openModal = () => {
+			modal.style.display = 'block';
+			document.body.style.overflow = 'hidden';
+		};
+
+		const closeModal = () => {
+			modal.style.display = 'none';
+			document.body.style.overflow = '';
+		};
+
+		triggers.forEach(trigger => {
+			trigger.addEventListener('click', e => {
 				if (e.target) {
 					e.preventDefault();
 				}
-
-				modal.style.display = 'block';
-				document.body.style.overflow = 'hidden';
-				// document.body.classList.add('modal-open'); // работает через bootstrap
+				openModal();
 			});
 		});
 
-		close.addEventListener('click', () => {
-			modal.style.display = 'none';
-			document.body.style.overflow = '';
-			// document.body.classList.remove('modal-open'); // работает через bootstrap
-		});
+		close.addEventListener('click', closeModal);
 
 		modal.addEventListener('click', e => {
 			if (e.target === modal) {
-				modal.style.display = 'none';
-				document.body.style.overflow = '';
-				// document.body.classList.remove('modal-open'); // работает через bootstrap
+				closeModal();
+			}
+		});
+
+		document.addEventListener('keydown', e => {
+			if (e.key === 'Escape') {
+				closeModal();
 			}
 		});
 	}
 
-	function showModalByTime(selector, time) {
+	const showModalByTime = (selector, time) => {
 		setTimeout(() => {
 			document.querySelector(selector).style.display = 'block';
 			document.body.style.overflow = 'hidden';
 		}, time);
-	}
+	};
 
 	bindModal(
 		'.popup_engineer_btn',
