223deb29eb8e71235baf031ef383c6847c523e67
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-11-22 11:51:52 +0300

    02_tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import './slider';
+import tabs from '../modules/tabs';
+
+window.addEventListener('DOMContentLoaded', () => {
+	tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+	tabs(
+		'.decoration_slider',
+		'.no_click',
+		'.decoration_content > div > div',
+		'after_click',
+	);
+});
